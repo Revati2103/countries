941e75cf6512df8be7408495fc88185fc8461ce5
Revati <revati.v.bordawekar@gmail.com>
2023-02-19 06:37:58 -0800

    added font awesome icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
 import './footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
         <span>
-    Made with ❤️ by <a href="https://github.com/Revati2103" target="_blank" rel='noreferrer'>Revati</a>
+    Made with <FontAwesomeIcon icon={faHeart} className='heart'/> by <a href="https://github.com/Revati2103" target="_blank" rel='noreferrer'>Revati</a>
 </span>
     </div>
   )
